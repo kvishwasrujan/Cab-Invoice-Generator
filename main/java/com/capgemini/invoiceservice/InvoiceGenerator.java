13d42bbcca502095563ab229fb427d2a9d2db973
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-22 15:33:57 +0530

    Cab-Invoice-Generator Functionality Added

---
+++ main/java/com/capgemini/invoiceservice/InvoiceGenerator.java
@@ -0,0 +1,14 @@
+package com.capgemini.invoiceservice;
+
+public class InvoiceGenerator {
+	private static final double COST_PER_KILOMETER = 10;
+	private static final int COST_PER_TIME = 1;
+	private static final double MINIMUM_FARE = 5;
+
+	public double calculateFare(double distance, int time) {
+		double totalFare = distance * COST_PER_KILOMETER + time * COST_PER_TIME;
+		if (totalFare < MINIMUM_FARE)
+			return MINIMUM_FARE;
+		return totalFare;
+	}
+}
---
+++ test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
@@ -0,0 +1,24 @@
+package com.capgemini.invoiceservice;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class InvoiceServiceTest  {
+	@Test
+	public void givenDistanceAndTimeShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+		double distance=2.0;
+		int time=5;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(25,fare,0.0);
+	}
+	@Test
+	public void givenLessDistanceOrTimeShouldReturnMinFare() {
+		InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
+		double distance=0.1;
+		int time=1;
+		double fare = invoiceGenerator.calculateFare(distance,time);
+		Assert.assertEquals(5,fare,0.0);
+	}
+}
