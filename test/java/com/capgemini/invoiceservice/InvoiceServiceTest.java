de0fd81b47fbb0f8371247be1cbf1b8809171e4e
kvishwasrujan <roohithbandi00@gmail.com>
2020-10-22 15:48:08 +0530

    Return Total Fare For Multiple Rides Functionality Added

--- test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
+++ test/java/com/capgemini/invoiceservice/InvoiceServiceTest.java
@@ -4,21 +4,34 @@ import org.junit.Test;
 
 import junit.framework.Assert;
 
-public class InvoiceServiceTest  {
+/**
+ * @author vishw
+ *
+ */
+public class InvoiceServiceTest {
 	@Test
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
-		InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
-		double distance=2.0;
-		int time=5;
-		double fare = invoiceGenerator.calculateFare(distance,time);
-		Assert.assertEquals(25,fare,0.0);
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(25, fare, 0.0);
 	}
+
 	@Test
 	public void givenLessDistanceOrTimeShouldReturnMinFare() {
-		InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
-		double distance=0.1;
-		int time=1;
-		double fare = invoiceGenerator.calculateFare(distance,time);
-		Assert.assertEquals(5,fare,0.0);
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		Assert.assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleRidesReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.08, 1) };
+		double fare = invoiceGenerator.calculateFare(rides);
+		Assert.assertEquals(30, fare, 0.0);
 	}
 }
